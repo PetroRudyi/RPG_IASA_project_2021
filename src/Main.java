647efbf6213ffaa030b26ed665ef2c062eb6d3b4
Petro Rudyi <petiaridd@gmail.com>
2021-11-18 20:17:15 +0200

    Update 1???

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import Generate.PerlinNoise;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        System.out.print("Point 1");
+        PerlinNoise noise = new PerlinNoise(400,400);
+        BufferedImage image = noise.getNoiseImage();
+        File output_pix = new File("C://Users//P//Desktop//image.jpg");
+        ImageIO.write(image, "jpg", output_pix);
     }
 }
