188ba7062c682976ead08d494f36d11b30f425d4
Sofia <sofi16616@gmail.com>
2021-12-10 23:32:49 +0200

    "Camera" (character is moving in the center of screen)

---
+++ src/Tile/TileManager.java
@@ -0,0 +1,75 @@
+package Tile;
+
+import Window.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class TileManager {
+    GamePanel gp;
+    Tile[] tile;
+    int mapTileNum[][];
+
+    public TileManager(GamePanel gp){
+        this.gp = gp;
+        tile = new Tile[10];
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+
+        getTileImage();
+    }
+    public void getTileImage(){
+        try{
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/tile2.png")));
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/Floor.webp")));
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/player/tile.jpeg")));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void loadMap(){
+        try{
+            InputStream is = getClass().getResourceAsStream("/images/player/testMap.txt");
+            assert is != null;
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            int col =0;
+            int row = 0;
+            while (col<gp.maxScreenCol && row<gp.maxScreenRow){
+                String line = br.readLine();
+                while(col<gp.maxScreenCol){
+                    String numbers[]=line.split(" ");
+                }
+            }
+        }catch(Exception e){
+
+        }
+    }
+    public void draw(Graphics2D g2){
+        g2.drawImage(tile[0].image, 0,0,gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[1].image, gp.tileSize,0,gp.tileSize, gp.tileSize, null);
+        g2.drawImage(tile[2].image, gp.tileSize*2,0,gp.tileSize, gp.tileSize, null);
+        int col = 0;
+        int row = 0;
+        int x=0;
+        int y=0;
+        while(col < gp.maxScreenCol && row < gp.maxScreenRow){
+            g2.drawImage(tile[0].image, x, y,gp.tileSize, gp.tileSize, null);
+        col++;
+        x+=gp.tileSize;
+        if(col==gp.maxScreenCol){
+            col =0;
+            x=0;
+            row++;
+            y+=gp.tileSize;
+        }
+        }
+    }
+}
